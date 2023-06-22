ba033a1567428d814d5ef8bfa98752cb910c49dc
eight04 <eight04@gmail.com>
2023-06-22 19:51:36 +0800

    Add: awaken mat searcher

---
+++ tools/awaken-material.mjs
@@ -0,0 +1,25 @@
+import * as fs from "fs/promises";
+import YAML from "yaml";
+
+const allCharacters = YAML.parse(await fs.readFile("characters.yml", "utf8"));
+const searchMat = [
+  // "退魔符文＜暗＞"
+  "高階繼承符文<木>"
+];
+const searchName = [
+  // "賽娜"
+];
+for (const c of allCharacters) {
+  if (!c.awakenPassives) continue;
+  if (searchName.length && searchName.every(k => !c.name.includes(k))) continue;
+  const map = new Map;
+  for (const p of c.awakenPassives) {
+    for (const match of p.materials.matchAll(/(.*)×\s*(\d+)/g)) {
+      const [, name, count] = match;
+      map.set(name.trim(), (map.get(name.trim()) ?? 0) + Number(count));
+    }
+  }
+  if (searchMat.length && searchMat.every(k => !map.has(k))) continue;
+  console.log(c.name);
+  console.log(map);
+}
